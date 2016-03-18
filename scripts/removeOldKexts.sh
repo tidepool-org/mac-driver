26443f8e5219c123bf1dc5744ab8c89aa5a4910c
Gerrit Niezen <gerrit.niezen@gmail.com>
2016-03-18 12:11:00 +0000

    updating Prolific driver to v1.6.0

--- scripts/removeOldKexts.sh
+++ scripts/removeOldKexts.sh
@@ -2,6 +2,8 @@
 
 # unload old extension
 sudo kextunload /Library/Extensions/sweetspot.DexComUSB.kext
+sudo kextunload /Library/Extensions/ProlificUsbSerial.kext
 
 # delete old extension
 sudo rm -rf /Library/Extensions/sweetspot.DexcomUSB.kext
+sudo rm -rf /Library/Extensions/ProlificUsbSerial.kext
